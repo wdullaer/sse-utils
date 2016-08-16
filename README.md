1d92637bcf2c677b28a7e5cfe7fc0e38f0de791e
Wouter Dullaert <wouter.dullaert@gmail.com>
2016-08-16 15:32:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# SSE Utils
+
+This is a small package that aims to be the server sent events equivalent of the JSON built-in module.
+It allows you to stringify an SSE object so it can be put on the wire.
+It allows you parse a stringified SSE object (you'll normally use another library to create an SSE client, but this can be helpfull for testing purposes).
+
+## Installation
+
+```bash
+npm install sse-utils
+```
+
+## Example
+
+```javascript
+let sse = require('sse-utils');
+
+let input = {foo: 'bar'};
+let sseString = sse.stringify(input);
+console.log(sseString);
+let output = sse.parse(input);
+console.log(output);
+```
+
+## API
+
+### sse.stringify(payload, serializer)
+Converts an object to an SSE compatible string
+
+* @param payload {object}    
+*data* {object} Object to be put in the data field. Uses some heuristics to properly seriaze this (`Buffer.toString('base64')`, `JSON.stringify`)  
+*id* {string?} The id of this message  
+*event* {string?} The event-type of this message  
+Additional attributes are omitted
+
+* @param serializer {function?} If the built-in serializers don't work for your data, you can pass in a custom one
+
+### sse.parse(message, deserializer)
+Converts a string into an SSE object
+
+* @param message {string}
+* @param deserializer {function?} If the built-in deserializer does not work for your data, you can pass your own
+* @return {object} The SSE object  
+*id* {string?} The id of this message  
+*event* {string?} The event-type of this message  
+*data* {string | object} The deserialized data  
+Additional attributes are added as strings on the object
+
+## TODO
+* Add support for asynchronous (de)serializers
---
+++ test/index.js
@@ -0,0 +1,145 @@
+let {expect} = require('chai')
+
+let sse = require('../lib')
+
+describe('stringify()', () => {
+  it('should stringify a string', () => {
+    const input = 'my-data'
+    const output = sse.stringify({data: input})
+    expect(output).to.be.a('string').that.equals(`data: ${input}\n\n`)
+  })
+
+  it('should stringify a string with newlines as multiple data lines', () => {
+    const input = 'my-data\nmy-other-data'
+    const output = sse.stringify({data: input})
+    expect(output).to.be.a('string').that.equals('data: my-data\ndata: my-other-data\n\n')
+  })
+
+  it('should stringify a Buffer', () => {
+    const input = Buffer.from('my-data')
+    const output = sse.stringify({data: input})
+    expect(output).to.be.a('string').that.equals(`data: ${input.toString('base64')}\n\n`)
+  })
+
+  it('should stringify an object', () => {
+    const input = {foo: 'bar'}
+    const output = sse.stringify({data: input})
+    expect(output).to.be.a('string').that.equals(`data: ${JSON.stringify(input)}\n\n`)
+  })
+
+  it('should stringify a number', () => {
+    const input = 2
+    const output = sse.stringify({data: input})
+    expect(output).to.be.a('string').that.equals('data: 2\n\n')
+  })
+
+  it('should set the correct event type', () => {
+    const input = 'my-data'
+    const eventType = 'my-event'
+    const output = sse.stringify({event: eventType, data: input})
+    expect(output).to.be.a('string').that.equals(`event: ${eventType}\ndata: ${input}\n\n`)
+  })
+
+  it('should set the correct id', () => {
+    const input = 'my-data'
+    const id = 'my-id'
+    const output = sse.stringify({id, data: input})
+    expect(output).to.be.a('string').that.equals(`id: ${id}\ndata: ${input}\n\n`)
+  })
+
+  it('should set all attributes', () => {
+    const input = 'my-data'
+    const eventType = 'my-event'
+    const id = 'my-id'
+    const output = sse.stringify({id, event: eventType, data: input})
+    expect(output).to.be.a('string').that.equals(`event: ${eventType}\nid: ${id}\ndata: ${input}\n\n`)
+  })
+
+  it('should use the serializer function', () => {
+    const input = 'my-data'
+    const serializer = () => 'serialized'
+    const output = sse.stringify({data: input}, serializer)
+    expect(output).to.be.a('string').that.equals('data: serialized\n\n')
+  })
+
+  it('should throw an error when data is not present', () => {
+    const stringify = sse.stringify.bind(null, {foo: 'bar'})
+    expect(stringify).to.throw(TypeError)
+  })
+
+  it('should throw an error when id is not a string', () => {
+    const id = {foo: 'bar'}
+    const stringify = sse.stringify.bind(null, {data: 'my-data', id})
+    expect(stringify).to.throw(TypeError)
+  })
+
+  it('should throw an error when event is not a string', () => {
+    const event = {foo: 'bar'}
+    const stringify = sse.stringify.bind(null, {data: 'my-data', event})
+    expect(stringify).to.throw(TypeError)
+  })
+
+  it('should throw an error when the serializer does not return a string', () => {
+    const serializer = () => ({foo: 'bar'})
+    const stringify = sse.stringify.bind(null, {data: 'my-data'}, serializer)
+    expect(stringify).to.throw(TypeError)
+  })
+})
+
+describe('parse()', () => {
+  it('should parse string data', () => {
+    const data = 'my-data'
+    const input = sse.stringify({data})
+    const output = sse.parse(input)
+    expect(output).to.be.an('object').that.deep.equals({data})
+  })
+
+  it('should parse multiple data lines', () => {
+    const data = 'my-data\nmy-other-data'
+    const input = sse.stringify({data})
+    const output = sse.parse(input)
+    expect(output).to.be.an('object').that.deep.equals({data})
+  })
+
+  it('should parse JSON data', () => {
+    const data = {foo: 'bar'}
+    const input = sse.stringify({data})
+    const output = sse.parse(input)
+    expect(output).to.be.an('object').that.has.property('data').which.deep.equals(data)
+  })
+
+  it('should parse JSON data that has newlines in the values', () => {
+    const data = {foo: 'bar\nbaz'}
+    const input = sse.stringify({data})
+    const output = sse.parse(input)
+    expect(output).to.be.an('object').that.has.property('data').which.deep.equals(data)
+  })
+
+  it('should parse using a deserializer function', () => {
+    const data = Buffer.from('my-data')
+    const input = sse.stringify({data})
+    const output = sse.parse(input, (payload) => Buffer.from(payload, 'base64').toString('utf-8'))
+    expect(output).to.be.an('object').that.has.property('data', 'my-data')
+  })
+
+  it('should return an empty object when passed undefined', () => {
+    const output = sse.parse()
+    expect(output).to.be.an('object').that.deep.equals({})
+  })
+
+  it('should return an empty object when passed an sse comment', () => {
+    const input = ':\n\n'
+    const output = sse.parse(input)
+    expect(output).to.be.an('object').that.deep.equals({})
+  })
+
+  it('should throw an error when the input is not a string', () => {
+    const parse = sse.parse.bind(null, {foo: 'bar'})
+    expect(parse).to.throw(TypeError)
+  })
+
+  it('should throw an error if the input does not end with 2 newlines', () => {
+    const parse = sse.parse.bind(null, 'my-data')
+    expect(parse).to.throw(TypeError)
+  })
+})
